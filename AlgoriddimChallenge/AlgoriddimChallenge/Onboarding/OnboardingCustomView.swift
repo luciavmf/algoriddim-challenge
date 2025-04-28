5c05c793a9453e4bb98c714f243f46dae19a7453
luciavmf <luciavmf@gmail.com>
2025-04-28 08:52:05 +0200

    Add ParticlesBackgroundView in the onboarding custom view

--- AlgoriddimChallenge/AlgoriddimChallenge/Onboarding/OnboardingCustomView.swift
+++ AlgoriddimChallenge/AlgoriddimChallenge/Onboarding/OnboardingCustomView.swift
@@ -12,10 +12,28 @@ final class OnboardingCustomView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        confiure()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+
+        confiure()
+    }
+
+    func confiure() {
+        let particles = ParticlesBackgroundView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        particles.translatesAutoresizingMaskIntoConstraints = false
+        particles.clipsToBounds = true
+        addSubview(particles)
+
+        NSLayoutConstraint.activate([
+            particles.leadingAnchor.constraint(equalTo: leadingAnchor),
+            particles.trailingAnchor.constraint(equalTo: trailingAnchor),
+            particles.topAnchor.constraint(equalTo: topAnchor),
+            particles.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
 }
 
